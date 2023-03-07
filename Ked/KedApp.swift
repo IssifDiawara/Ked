7bfb77ac84b75cabaa6af3b1d0fe36563e7216f4
Issif Diawara <issifdiawarapro@gmail.com>
2023-03-07 14:46:18 +0100

    Initial Commit

---
+++ Ked/KedApp.swift
@@ -0,0 +1,17 @@
+//
+//  KedApp.swift
+//  Ked
+//
+//  Created by Issif DIAWARA on 07/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct KedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
